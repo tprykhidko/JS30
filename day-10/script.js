151ff54ced3e6e95ed2fd8d046f7e5bdf09271e1
tprykhidko <tetiana_prykhidko@ukr.net>
2023-07-17 19:06:34 +0300

    add 'hold shift and check all' items type list

---
+++ day-10/script.js
@@ -0,0 +1,24 @@
+const checkBoxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkBoxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", handleCheck);
+});
